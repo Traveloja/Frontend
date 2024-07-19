9bc11e3d4a1472094e418db0a35f60b4d1916d6d
Christopher Ade W. <111300719+caw083@users.noreply.github.com>
2024-07-19 14:27:59 +0700

    Add files via upload

---
+++ Traveloja_Final/js/main.js
@@ -0,0 +1,4 @@
+document.getElementById('navbar-toggler').addEventListener('click', function() {
+    var menuWrapper = document.getElementById('navbar-menu-wrapper');
+    menuWrapper.classList.toggle('show');
+});
